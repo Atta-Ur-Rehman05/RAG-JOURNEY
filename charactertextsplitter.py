20a5ead7983b0f2184256ee752628d2cc9491b5e
ATTA UR REHMAN <attaur5565@gmail.com>
2026-07-22 11:26:15 +0500

    docs: add documentation regarding the limitations of CharacterTextSplitter

--- charactertextsplitter.py
+++ charactertextsplitter.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import CharacterTextSplitter
 text = """
         microsoft is a compnay 
 
-        
+
         they were established in 1900.
 
 
@@ -28,3 +28,18 @@ for i,chunk in enumerate(chunks):
     print("chunk no ",i+1)
     print(chunk)
     print("\n")
+
+
+    # draw backs of charactertextsplitter
+    # it does not support multiple separator 
+    # 1. it split the text on the basis of characters
+    # 2. it does not consider the meaning of the text
+    # 3. it does not consider the context of the text
+    # 4. it does not consider the structure of the text
+    # 5. it does not consider the semantic meaning of the text
+    # 6. it does not consider the syntactic meaning of the text
+    # 7. it does not consider the pragmatic meaning of the text
+    # 8. it does not consider the semantic meaning of the text
+    # 9. it does not consider the syntactic meaning of the text
+    # 10. it does not consider the pragmatic meaning of the text
+    # 
