0647b9eaea8873bfe351885dbc0d60a5e9a4aa26
ATTA UR REHMAN <attaur5565@gmail.com>
2026-07-22 11:30:18 +0500

    feat: replace CharacterTextSplitter with RecursiveCharacterTextSplitter for improved text chunking

--- charactertextsplitter.py
+++ charactertextsplitter.py
@@ -2,6 +2,8 @@
 
 from langchain_text_splitters import CharacterTextSplitter
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 text = """
         microsoft is a compnay 
 
@@ -13,21 +15,21 @@ text = """
 """
 
 
-text_splitter = CharacterTextSplitter(
-    separator="\n\n",
-    chunk_size=20,
-    chunk_overlap=5,
-    length_function=len,
-    is_separator_regex=False,
-)
+# text_splitter = CharacterTextSplitter(
+#     separator="\n\n",
+#     chunk_size=20,
+#     chunk_overlap=5,
+#     length_function=len,
+#     is_separator_regex=False,
+# )
 
-chunks = text_splitter.split_text(text)
+# chunks = text_splitter.split_text(text)
 
-for i,chunk in enumerate(chunks):
-    print(len(chunk))
-    print("chunk no ",i+1)
-    print(chunk)
-    print("\n")
+# for i,chunk in enumerate(chunks):
+#     print(len(chunk))
+#     print("chunk no ",i+1)
+#     print(chunk)
+#     print("\n")
 
 
     # draw backs of charactertextsplitter
@@ -43,3 +45,32 @@ for i,chunk in enumerate(chunks):
     # 9. it does not consider the syntactic meaning of the text
     # 10. it does not consider the pragmatic meaning of the text
     # 
+
+text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n\n", "\n", ".", " "],
+        chunk_size=20,
+        chunk_overlap=5,
+        length_function=len,
+        is_separator_regex=False,
+    )
+
+chunks = text_splitter.split_text(text)
+
+for i,chunk in enumerate(chunks):
+    print(len(chunk))
+    print("chunk no ",i+1)
+    print(chunk)
+    print("\n")
+
+    # advantages of recursive character text splitter
+    # 1. it support multiple separator 
+    # 2. it split the text on the basis of characters
+    # 3. it does not consider the meaning of the text
+    # 4. it does not consider the context of the text
+    # 5. it does not consider the structure of the text
+    # 6. it does not consider the semantic meaning of the text
+    # 7. it does not consider the syntactic meaning of the text
+    # 8. it does not consider the pragmatic meaning of the text
+    # 9. it does not consider the semantic meaning of the text
+    # 10. it does not consider the syntactic meaning of the text
+    # 11. it does not consider the pragmatic meaning of the text
